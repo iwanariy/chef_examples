558e85831c4e3d550a12723111f1516f4c3a78b6
narikin <yuki.iwnr@gmail.com>
2016-07-09 11:35:02 +0900

    fix user recipe

--- cookbooks/centos/attributes/user.rb
+++ cookbooks/centos/attributes/user.rb
@@ -1,3 +1,4 @@
 force_override['centos']['user']['name'] = 'admin'
+force_override['centos']['user']['group'] = 'wheel'
 force_override['centos']['user']['home'] = '/home/admin'
 force_override['centos']['user']['shell'] = '/bin/bash'
--- cookbooks/centos/recipes/user.rb
+++ cookbooks/centos/recipes/user.rb
@@ -1,4 +1,5 @@
 user node['centos']['user']['name'] do
+  group node['centos']['user']['group']
   home node['centos']['user']['home']
   shell node['centos']['user']['shell']
   supports manage_home: true
