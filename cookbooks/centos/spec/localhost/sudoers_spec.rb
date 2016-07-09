951581e0bcbf3123e146d4ebade114d40f3a4a88
narikin <yuki.iwnr@gmail.com>
2016-07-09 15:16:18 +0900

    fix spec for sudoers

--- cookbooks/centos/spec/localhost/sudoers_spec.rb
+++ cookbooks/centos/spec/localhost/sudoers_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 describe file('/etc/sudoers') do
-  its(:content) { should match /%wheel  ALL=(ALL)/ }
+  its(:content) { should match /%wheel  ALL=(ALL)       ALL/ }
   it { should be_mode 440 }
 end
