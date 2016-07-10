2b618f68cc526778750c321a86ca3a493439b7dc
narikin <yuki.iwnr@gmail.com>
2016-07-10 22:10:59 +0900

    add spec to check port

--- cookbooks/centos/spec/localhost/sshd_spec.rb
+++ cookbooks/centos/spec/localhost/sshd_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe file('/etc/ssh/sshd_config') do
+  its(:content) { should match /Port 2222/ }
   its(:content) { should match /PasswordAuthentication no/ }
   it { should be_mode 600 }
 end
