53b9a39a4c8bc34da723ea7aae23ec4cd71aa172
narikin <yuki.iwnr@gmail.com>
2016-07-10 22:29:07 +0900

    add notifies into sshd_config recipe

--- cookbooks/centos/recipes/sshd_config.rb
+++ cookbooks/centos/recipes/sshd_config.rb
@@ -1,3 +1,8 @@
+service "sshd" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
 template '/etc/ssh/sshd_config' do
   source 'sshd_config.erb'
   owner 'root'
@@ -7,4 +12,5 @@ template '/etc/ssh/sshd_config' do
     :port => node["centos"]["sshd_config"]["port"],
     :PasswordAuthentication => node["centos"]["sshd_config"]["PasswordAuthentication"]
   })
+  notifies :reload, "service[sshd]"
 end
