4e9bd62970066f4b3062815d5b6cc32d7b6d2127
narikin <yuki.iwnr@gmail.com>
2016-09-02 23:53:54 +0900

    change attributes level into default

--- cookbooks/centos/attributes/sshd_config.rb
+++ cookbooks/centos/attributes/sshd_config.rb
@@ -1,5 +1,5 @@
 # change ssh port
-force_override['centos']['sshd_config']['port'] = '2222'
+default['centos']['sshd_config']['port'] = '2222'
 
 # forbid password login
-force_override['centos']['sshd_config']['PasswordAuthentication'] = 'no'
+default['centos']['sshd_config']['PasswordAuthentication'] = 'no'
--- cookbooks/centos/attributes/user.rb
+++ cookbooks/centos/attributes/user.rb
@@ -1,4 +1,4 @@
-force_override['centos']['user']['name'] = 'admin'
-force_override['centos']['user']['group'] = 'wheel'
-force_override['centos']['user']['home'] = '/home/admin'
-force_override['centos']['user']['shell'] = '/bin/bash'
+default['centos']['user']['name'] = 'admin'
+default['centos']['user']['group'] = 'wheel'
+default['centos']['user']['home'] = '/home/admin'
+default['centos']['user']['shell'] = '/bin/bash'
